4728f791310da943b106cd9279d35e87f91ad5ed
Artur Stepniewski <artur@meniga.com>
2020-05-08 17:47:16 +0200

    Stop using flutter version in favor of git

---
+++ lib/src/tag/tag.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:pub_semver/pub_semver.dart';
+import 'package:quiver/check.dart';
+
+class Tag extends Equatable {
+  final String value;
+
+  Tag(this.value) {
+    checkNotNull(value);
+  }
+
+  @override
+  List<Object> get props => [];
+
+  @override
+  bool get stringify => true;
+
+  Version get version {
+    try {
+      if (value.startsWith("v")) {
+        return Version.parse(value.substring(1));
+      } else {
+        return Version.parse(value);
+      }
+    } catch (_) {
+      return Version.none;
+    }
+  }
+}
---
+++ lib/src/tag/tag_not_found_exception.dart
@@ -0,0 +1,9 @@
+import 'package:pub_semver/src/version_constraint.dart';
+
+class TagNotFoundException {
+  final VersionConstraint _constraint;
+  TagNotFoundException(this._constraint);
+
+  @override
+  String toString() => "Tag not found for ${_constraint.toString()} constraint";
+}
